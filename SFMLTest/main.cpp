5140e9de3cf1bcb5cf6f09d595f7299925e7e676
Ellyx Jolley <ellyxjolley@gmail.com>
2015-12-12 20:10:58 -0700

    Login state initial

--- SFMLTest/main.cpp
+++ SFMLTest/main.cpp
@@ -17,11 +17,11 @@ int main()
 
 	Game game;
 	
-	//game.pushState(new GameStateLogin(&game));
+	game.pushState(new GameStateLogin(&game));
 	//game.pushState(new MenuState(&game));
 	//game.pushState(new LessonState(&game));
 	//game.pushState(new QuizState(&game));
-	game.pushState(new TeacherState(&game));
+	//game.pushState(new TeacherState(&game));
 	//game.pushState(new ErrorState(&game, "token"));
 	game.gameLoop();
 	
